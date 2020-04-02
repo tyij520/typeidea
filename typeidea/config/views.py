651fb1c9fc2a488b88b102cb9b0f5922cab5aadd
Tye <tyij520@sohu.com>
2020-04-02 15:06:58 +0800

    添加友链功能

--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -1,15 +1,24 @@
 '''
 @Author: Tye
 @Date: 2020-03-23 23:56:58
-@LastEditTime: 2020-03-25 14:48:23
+@LastEditTime: 2020-04-02 14:21:13
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
-@FilePath: \typeidea\typeidea\config\views.py
 '''
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# 引入公共视图类
+from django.views.generic import ListView
+from blog.views import CommonViewMixin      
+
+from .models import Link
+
+
+
 # Create your views here.
-def links(request):
-    return HttpResponse('links')
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -1,7 +1,7 @@
 '''
 @Author: Tye
 @Date: 2020-03-23 14:51:44
-@LastEditTime: 2020-04-01 20:23:19
+@LastEditTime: 2020-04-02 14:43:32
 @Description: 
 '''
 
@@ -11,7 +11,7 @@ from django.contrib import admin
 from blog.views import (
     IndexView, CategoryView, TagView, SearchView, AuthorView, PostDetailView
 )
-from config.views import links
+from config.views import LinkListView
 from .custom_site import custom_site
 
 
@@ -21,9 +21,9 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author-list'),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),  # 配置自定义站点
 ]
